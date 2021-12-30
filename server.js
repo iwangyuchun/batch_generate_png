e8d210afe57eadf7dcec34e6ffc36a71632f2ef4
WangYunchun <1140145488@qq.com>
2021-12-30 15:55:35 +0800

    update package

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-let express=require('express');
-const path=require('path');
+let express = require('express');
+const path = require('path');
+const open = require('open');
 
-let app=express();
+let app = express();
 
-app.use(express.static(path.join(__dirname,'views')));
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.static(path.join(__dirname,'data')));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'data')));
 
-app.listen(8088,'0.0.0.0',function(){
-    console.log("start server on 3000");
+app.listen(8088, '0.0.0.0', function () {
+    console.log("start server on 8088");
+    // 使用默认浏览器打开
+    open('http://localhost:8088');
 })
 
 
